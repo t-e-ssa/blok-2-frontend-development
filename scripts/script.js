48ce34f0994912bfcd4fe20316177e877b60f5dd
t-e-ssa <145226466+t-e-ssa@users.noreply.github.com>
2024-11-27 15:46:42 +0100

    aanpassingen

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
-// JavaScript Document
-console.log("hi");
+console.log("Slay");
+
+var openButton = document.querySelector("header button");
+var sluitButton = document.querySelector("nav button");
+
+openButton.onclick = openMenu;
+sluitButton.onclick = sluitMenu;
+
+
+function openMenu() {  
+  var deNav = document.querySelector("nav");
+  deNav.classList.add("toonMenu");
+}
+
+function sluitMenu() {
+  var deNav = document.querySelector("nav");
+  deNav.classList.remove("toonMenu");
+}
