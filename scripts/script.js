9bead83678711651ed48e0c085b459da2dedefa4
t-e-ssa <145226466+t-e-ssa@users.noreply.github.com>
2024-12-01 17:13:04 +0100

    javascript onderdelen toegevoegd en pagina 2 begonnen

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ console.log("Slay");
 
 var openButton = document.querySelector("header button");
 var sluitButton = document.querySelector("nav button");
+var linkTekst = document.querySelector("#aanpasbare_linktekst")
 
 openButton.onclick = openMenu;
 sluitButton.onclick = sluitMenu;
@@ -16,3 +17,15 @@ function sluitMenu() {
   var deNav = document.querySelector("nav");
   deNav.classList.remove("toonMenu");
 }
+
+function veranderLinkTekst () {
+    if (linkTekst.textContent == "Dit zijn gekoelde klassiekers") {
+      linkTekst.textContent = "Dit is de tripleshot espresso";
+    } else if (linkTekst.textContent == "Dit is de tripleshot espresso") {
+      linkTekst.textContent = "Dit is de frappeccino";
+    } else {
+      linkTekst.textContent = "Dit zijn gekoelde klassiekers";
+    }
+}
+
+setInterval (veranderLinkTekst, 4000);
