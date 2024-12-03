2c5dd78ba8c3c7158bb84f5811fddaa3036e015a
t-e-ssa <145226466+t-e-ssa@users.noreply.github.com>
2024-12-03 20:49:02 +0100

    aanpassingen

--- scripts/script.js
+++ scripts/script.js
@@ -29,3 +29,75 @@ function veranderLinkTekst () {
 }
 
 setInterval (veranderLinkTekst, 6500);
+
+
+
+/* Hieronder wordt de code van de site: https://codepen.io/shooft/pen/mdBOZLz gebruikt ter inspiratie voor de carrousel. */
+
+function createCaroCarrousel(carrouselID) {
+	let carrousel = document.querySelector("#"+carrouselID);
+  let carrouselElementsContainer = carrousel.querySelector(":scope > ul");
+	let carrouselElements = carrouselElementsContainer.querySelectorAll("li");
+  let linkButtons = carrousel.querySelectorAll(":scope > button");
+  
+  function iniLinkButtons() {    
+    for(linkButton of linkButtons) {
+      linkButton.addEventListener("click", function(e){
+        e.preventDefault();
+        let direction = this.classList.contains("previous");
+        goToElement(direction);
+      });
+    }
+  }
+  
+  function iniStartPosition() {
+    carrouselElements[0].classList.add("current");
+    carrouselElementsContainer.scrollLeft = 0;
+  }
+  
+  function goToElement(direction) {
+		let currentElement = carrousel.querySelector(":scope > ul > .current");
+
+		let newElement;
+		if (direction == "previous") {
+			newElement = currentElement.previousElementSibling;
+			
+      if (!newElement) {
+				newElement = carrousel.querySelector(":scope > ul > li:last-of-type");
+			}
+		} else {
+			newElement = currentElement.nextElementSibling;
+			if (!newElement) {
+				newElement = carrousel.querySelector(":scope > ul > li:first-of-type");
+			}
+		}
+
+		scrollToElement(newElement);
+  }
+  
+  function scrollToElement(newElement) {
+    let carouselElementsContainer = newElement.closest("ul");
+		let newElementOffset = newElement.offsetLeft;
+		
+		carouselElementsContainer.scrollTo({
+			left: newElementOffset
+		});
+    
+    updateCurrentElement(newElement);
+  }
+  
+	function updateCurrentElement(newElement) {
+		let currentElement = carrousel.querySelector(":scope > ul > .current");
+		currentElement.classList.remove("current");
+		newElement.classList.add("current");
+	}
+  
+  iniLinkButtons();	
+  iniStartPosition();
+}
+
+(function() {
+  createCaroCarrousel("carrousel1");
+})();
+
+/* Einde van de gebruikte site ter inspiratie voor de carrousel */
